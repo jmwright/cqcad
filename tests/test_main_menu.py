da8898b76d8dd6c1294636411ffdc2020cd269b7
Jeremy Mack Wright <wrightjmf@gmail.com>
2017-11-13 21:21:35 -0500

    Added a tests for the dock visibility per #7

--- tests/test_main_menu.py
+++ tests/test_main_menu.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import pytest
+import sys, os
 from PyQt5 import QtCore
 from ..cqcad import CQCADGui
 
+@pytest.fixture(scope="session", autouse=True)
+def do_something(request):
+    # Make sure that these tests can import the cqcad modules properly
+    encoding = sys.getfilesystemencoding()
+    test_base_path = os.path.dirname(unicode(__file__, encoding))
+    module_base_path = os.path.abspath(os.path.join(test_base_path, os.pardir))
+    sys.path.insert(0, module_base_path)
+
+    # request.addfinalizer(finalizer_function)
+
 def test_menu_navigation(qtbot, tmpdir):
     '''
-    test to ensure basic find files functionality is working.
+    test to ensure basic menu navigation and operation is working
     '''
     window = CQCADGui()
     window.show()
@@ -19,3 +30,10 @@ def test_menu_navigation(qtbot, tmpdir):
     # Test the examples menu
     qtbot.mouseClick(window.examplesMenu, QtCore.Qt.LeftButton)
     assert len(window.examplesMenu.actions()) == 0  # TODO: Update this when the CadQuery library is integrated
+
+    # Test the dock and its visibility menu checkbox
+    dockActChecked = window.dockAct.isChecked()
+    assert(dockActChecked == window.dock.isVisible())
+    window.dockAct.trigger()
+    assert(window.dockAct.isChecked() is not dockActChecked)
+    assert(window.dock.isVisible() is not dockActChecked)
